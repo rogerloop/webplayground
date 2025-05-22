ef2811d0ec8774a23dc080a0f6297d414e3356bb
GitHub Desktop RGR <bascular-rendiment0x@icloud.com>
2025-05-23 00:14:04 +0200

    Update home template to use dynamic title and refine HomePageView

--- core/views.py
+++ core/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "core/home.html"
-
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'title': 'Mi Gran proyercto Django Webplayground'})
+    
 class SamplePageView(TemplateView):
     template_name = "core/sample.html"
     
